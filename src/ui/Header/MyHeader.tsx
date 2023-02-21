d7e84ecc5a5c1b69b6bb44dfc248932949c30cc0
홍석기 <skhong@eco.kr>
2023-02-21 17:12:20 +0900

    react-router 추가

--- src/ui/Header/MyHeader.tsx
+++ src/ui/Header/MyHeader.tsx
@@ -1,4 +1,21 @@
 import { Desktop, Tablet, Mobile } from '@assets/mediaQuery'
-function MyHeader() {}
+import { Link } from 'react-router-dom'
+import { UserOutlined, MenuOutlined } from '@ant-design/icons'
+import './MyHeader.scss'
+function MyHeader() {
+  return (
+    <div className="my-header">
+      <div className="container flex-center-left">
+        <Mobile>
+          <MenuOutlined className="menu-icon flex-center" />
+        </Mobile>
+        <b>쉐어</b>
+        <Link to="/login" className="mla login-icon flex-center">
+          <UserOutlined />
+        </Link>
+      </div>
+    </div>
+  )
+}
 
 export default MyHeader
---
+++ src/ui/Login/Login.tsx
@@ -0,0 +1,10 @@
+import './Login.scss'
+function Login() {
+  return (
+    <div className="login flex-center">
+      <div className="container"></div>
+    </div>
+  )
+}
+
+export default Login
